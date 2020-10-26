3fee5835bfd08318aae9fee5a079dfe126f6c81c
SantiagoToledo <santi.nt2@gmail.com>
2020-10-25 22:29:50 -0300

    total likes and test for it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = () => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	if (Array.isArray(blogs)) {
+		return blogs.reduce((sum, blog) => (sum = sum + blog.likes), 0)
+	} else {
+		return blogs.likes
+	}
+}
+
 module.exports = {
 	dummy,
+	totalLikes,
 }
