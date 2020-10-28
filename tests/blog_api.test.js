b8d63a16cfe133c691419a2edc52ad6e334d9960
SantiagoToledo <santi.nt2@gmail.com>
2020-10-28 19:20:46 -0300

    added @type jest refactored and test post with async

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,6 +29,36 @@ test('get all blogs in db', async () => {
 	expect(blogs.body).toEqual(blogsAfterProcess)
 })
 
+test('verifies blogs have property id', async () => {
+	const blogs = await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	expect(blogs.body[0].id).toBeDefined()
+})
+
+test('verfies post creates blog', async () => {
+	const newBlog = {
+		title: 'Testing for blog post',
+		author: 'Santiago Tester',
+		url: 'fullstackIsCool.com',
+		likes: 999,
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	const blogsAfter = await await helper.blogsInDb()
+	const blogsTitle = blogsAfter.map((b) => b.title)
+
+	expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
+	expect(blogsTitle).toContainEqual(newBlog.title)
+})
+
 afterAll(() => {
 	mongoose.connection.close()
 })
